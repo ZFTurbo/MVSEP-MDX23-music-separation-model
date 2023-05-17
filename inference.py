5a13270c1229b88f53ca7e2c73eecbe114b60c6b
user-miner1 <amvnews@mail.ru>
2023-05-18 00:40:46 +0300

    Added 2nd instrumental variant

--- inference.py
+++ inference.py
@@ -4,7 +4,7 @@ __author__ = 'https://github.com/ZFTurbo/'
 if __name__ == '__main__':
     import os
 
-    gpu_use = "1"
+    gpu_use = "0"
     print('GPU use: {}'.format(gpu_use))
     os.environ["CUDA_VISIBLE_DEVICES"] = "{}".format(gpu_use)
 
